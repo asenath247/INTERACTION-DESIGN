756f3d0c786ba3bc19af986328c1cd36e8d934fb
Asenath Antwi <55785965+asenath247@users.noreply.github.com>
2021-01-08 15:34:28 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import myGIFlogo from './components/HamburgerMenu/assets/myGIFlogo.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={myGIFlogo} className="App-logo" alt="myGIFlogo" />
+        <p>
+          Welcome 
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
